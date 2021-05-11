b0e42e0c8baa543f630f725b608ee1ae5610e6a0
Amipower <71679909+Amipower@users.noreply.github.com>
2021-05-11 16:03:24 +0800

    金字塔and方塊

---
+++ 3D/Assets/Scrips/Bouncing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public Vector3 force;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Kiss");
+        rb.AddForce(force);
+    }
+}
---
+++ 3D/Assets/Scrips/rotatecube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class rotatecube : MonoBehaviour
+{
+    public Vector3 EditRotate;
+
+    void Start()
+    { }
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            transform.Rotate(2, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Rotate(-1, 3, -1);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0, 3);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0, 0, 0.01f);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0, 0, -0.01f);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+    }
+       
+
+}
